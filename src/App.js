cabbb85cf18d252a9b32558cebcfd14433917c52
xs2tariqrasheed <xs2tariqrasheed@gmail.com>
2021-05-09 08:06:48 +0500

    even card height fixed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import dayjs from "dayjs";
 import React, { useEffect } from "react";
 import { Button, Card, Row, Col } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 
-import { EditFilled } from "@ant-design/icons";
+import { EditFilled, PlusOutlined } from "@ant-design/icons";
 
 import { sagaActions } from "./sagaActions";
 
 import "./App.css";
+
+var relativeTime = require("dayjs/plugin/relativeTime");
+dayjs.extend(relativeTime);
+dayjs().format();
+
+const { Meta } = Card;
 
 function App() {
   const dispatch = useDispatch();
@@ -17,7 +24,7 @@ function App() {
   }, [dispatch]);
 
   return (
-    <div>
+    <div style={{ margin: 50 }}>
       <Row gutter={20}>
         {todos.map((todo) => (
           <Col span={6} key={todo.id}>
@@ -29,10 +36,29 @@ function App() {
                 </Button>
               }
             >
-              {todo.description}
+              <div className="todo-description">{todo.description}</div>
+
+              <Meta
+                style={{ textAlign: "center" }}
+                description={dayjs(todo.created_at).fromNow()}
+              />
             </Card>
           </Col>
         ))}
+        <Col span={6}>
+          <Card style={{ textAlign: "center" }}>
+            <PlusOutlined
+              style={{
+                fontSize: 70,
+                marginBottom: 47,
+                marginTop: 20,
+                color: "grey",
+              }}
+            />
+
+            <Meta description="Create Todo" />
+          </Card>
+        </Col>
       </Row>
     </div>
   );
